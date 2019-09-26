14a3cf625dc71cfa810d19c1e0d43d66131f9b6b
Cristina Sturm <cristurm90@gmail.com>
2019-09-26 16:45:25 -0300

    loops through an array of day periods

--- src/DayRows.js
+++ src/DayRows.js
@@ -1,22 +1,19 @@
 import React from 'react'
 import WEEK_DAYS from './WeekDays'
+import DAY_PERIODS from './DayPeriods'
+
+const buildKey = (weekDay, dayPeriod) => weekDay.shortName + "-" + dayPeriod.shortName
 
 export default () =>
   WEEK_DAYS.map((weekDay) => (
     <tr>
       <th scope="row"><abbr title={weekDay.longName}>{weekDay.abbreviatedName}</abbr></th>
-      <td>
-        <input type="checkbox" name="disponibilidade" value={weekDay.shortName + "-manha"} id={weekDay.shortName + "-manha"} />
-        <label for={weekDay.shortName + "-manha"} class="screen-reader-only">Manhã, {weekDay.shortName}</label>
-      </td>
-      <td>
-        <input type="checkbox" name="disponibilidade" value={weekDay.shortName + "-tarde"} id={weekDay.shortName + "-tarde"} />
-        <label for={weekDay.shortName + "-tarde"} class="screen-reader-only">Tarde, {weekDay.shortName}</label>
-      </td>
-      <td>
-        <input type="checkbox" name="disponibilidade" value={weekDay.shortName + "-noite"} id={weekDay.shortName + "-noite"} />
-        <label for={weekDay.shortName + "-noite"} class="screen-reader-only">Noite, {weekDay.shortName}</label>
-      </td>
+      {DAY_PERIODS.map((dayPeriod) => (
+        <td>
+          <input type="checkbox" name="disponibilidade" value={buildKey(weekDay, dayPeriod)} id={buildKey(weekDay, dayPeriod)} />
+          <label for={buildKey(weekDay, dayPeriod)} class="screen-reader-only">{weekDay.longName  + ", " + dayPeriod.longName}</label>
+        </td>
+      ))}
     </tr>
   )
 )
