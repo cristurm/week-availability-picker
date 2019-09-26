77bee8c253573a0ddfbe8e99b66d0556b641f273
Cristina Sturm <cristurm90@gmail.com>
2019-09-26 16:01:06 -0300

    availability initial parse

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css'
 const parseData = (event) => {
   event.preventDefault()
 
-  console.log(event.target)
+  const formData = new FormData(event.target)
+  alert(formData.getAll("disponibilizade"))
 }
 
 const App = () => (
@@ -24,105 +25,105 @@ const App = () => (
           <tr>
             <th scope="row"><abbr title="Segunda-feira">Seg.</abbr></th>
             <td>
-              <input type="checkbox" name="segunda-manha" value="segunda-manha" id="segunda-manha" />
+              <input type="checkbox" name="disponibilizade" value="segunda-manha" id="segunda-manha" />
               <label for="segunda-manha" class="screen-reader-only">Manhã, segunda</label>
             </td>
             <td>
-              <input type="checkbox" name="segunda-tarde" value="segunda-tarde" id="segunda-tarde" />
+              <input type="checkbox" name="disponibilizade" value="segunda-tarde" id="segunda-tarde" />
               <label for="segunda-tarde" class="screen-reader-only">Tarde, segunda</label>
             </td>
             <td>
-              <input type="checkbox" name="segunda-noite" value="segunda-noite" id="segunda-noite" />
+              <input type="checkbox" name="disponibilizade" value="segunda-noite" id="segunda-noite" />
               <label for="segunda-noite" class="screen-reader-only">Noite, segunda</label>
             </td>
           </tr>
           <tr>
             <th scope="row"><abbr title="Terça-feira">Ter.</abbr></th>
             <td>
-              <input type="checkbox" name="terca-manha" value="terca-manha" id="terca-manha" />
+              <input type="checkbox" name="disponibilizade" value="terca-manha" id="terca-manha" />
               <label for="terca-manha" class="screen-reader-only">Manhã, terça</label>
             </td>
             <td>
-              <input type="checkbox" name="terca-tarde" value="terca-tarde" id="terca-tarde" />
+              <input type="checkbox" name="disponibilizade" value="terca-tarde" id="terca-tarde" />
               <label for="terca-tarde" class="screen-reader-only">Tarde, terça</label>
             </td>
             <td>
-              <input type="checkbox" name="terca-noite" value="terca-noite" id="terca-noite" />
+              <input type="checkbox" name="disponibilizade" value="terca-noite" id="terca-noite" />
               <label for="terca-noite" class="screen-reader-only">Noite, terça</label>
             </td>
           </tr>
           <tr>
             <th scope="row"><abbr title="Quarta-feira">Qua.</abbr></th>
             <td>
-              <input type="checkbox" name="quarta-manha" value="quarta-manha" id="quarta-manha" />
+              <input type="checkbox" name="disponibilizade" value="quarta-manha" id="quarta-manha" />
               <label for="quarta-manha" class="screen-reader-only">Manhã, quarta</label>
             </td>
             <td>
-              <input type="checkbox" name="quarta-tarde" value="quarta-tarde" id="quarta-tarde" />
+              <input type="checkbox" name="disponibilizade" value="quarta-tarde" id="quarta-tarde" />
               <label for="quarta-tarde" class="screen-reader-only">Tarde, quarta</label>
             </td>
             <td>
-              <input type="checkbox" name="quarta-noite" value="quarta-noite" id="quarta-noite" />
+              <input type="checkbox" name="disponibilizade" value="quarta-noite" id="quarta-noite" />
               <label for="quarta-noite" class="screen-reader-only">Noite, quarta</label>
             </td>
           </tr>
           <tr>
             <th scope="row"><abbr title="Quinta-feira">Qui.</abbr></th>
             <td>
-              <input type="checkbox" name="quinta-manha" value="quinta-manha" id="quinta-manha" />
+              <input type="checkbox" name="disponibilizade" value="quinta-manha" id="quinta-manha" />
               <label for="quinta-manha" class="screen-reader-only">Manhã, quinta</label>
             </td>
             <td>
-              <input type="checkbox" name="quinta-tarde" value="quinta-tarde" id="quinta-tarde" />
+              <input type="checkbox" name="disponibilizade" value="quinta-tarde" id="quinta-tarde" />
               <label for="quinta-tarde" class="screen-reader-only">Tarde, quinta</label>
             </td>
             <td>
-              <input type="checkbox" name="quinta-noite" value="quinta-noite" id="quinta-noite" />
+              <input type="checkbox" name="disponibilizade" value="quinta-noite" id="quinta-noite" />
               <label for="quinta-noite" class="screen-reader-only">Noite, quinta</label>
             </td>
           </tr>
           <tr>
             <th scope="row"><abbr title="Sexta-feira">Sex.</abbr></th>
             <td>
-              <input type="checkbox" name="sexta-manha" value="sexta-manha" id="sexta-manha" />
+              <input type="checkbox" name="disponibilizade" value="sexta-manha" id="sexta-manha" />
               <label for="sexta-manha" class="screen-reader-only">Manhã, sexta</label>
             </td>
             <td>
-              <input type="checkbox" name="sexta-tarde" value="sexta-tarde" id="sexta-tarde" />
+              <input type="checkbox" name="disponibilizade" value="sexta-tarde" id="sexta-tarde" />
               <label for="sexta-tarde" class="screen-reader-only">Tarde, sexta</label>
             </td>
             <td>
-              <input type="checkbox" name="sexta-noite" value="sexta-noite" id="sexta-noite" />
+              <input type="checkbox" name="disponibilizade" value="sexta-noite" id="sexta-noite" />
               <label for="sexta-noite" class="screen-reader-only">Noite, sexta</label>
             </td>
           </tr>
           <tr>
             <th scope="row"><abbr title="Sábado">Sáb.</abbr></th>
             <td>
-              <input type="checkbox" name="sabado-manha" value="sabado-manha" id="sabado-manha" />
+              <input type="checkbox" name="disponibilizade" value="sabado-manha" id="sabado-manha" />
               <label for="sabado-manha" class="screen-reader-only">Manhã, sábado</label>
             </td>
             <td>
-              <input type="checkbox" name="sabado-tarde" value="sabado-tarde" id="sabado-tarde" />
+              <input type="checkbox" name="disponibilizade" value="sabado-tarde" id="sabado-tarde" />
               <label for="sabado-tarde" class="screen-reader-only">Tarde, sábado</label>
             </td>
             <td>
-              <input type="checkbox" name="sabado-noite" value="sabado-noite" id="sabado-noite" />
+              <input type="checkbox" name="disponibilizade" value="sabado-noite" id="sabado-noite" />
               <label for="sabado-noite" class="screen-reader-only">Noite, sábado</label>
             </td>
           </tr>
           <tr>
             <th scope="row"><abbr title="Domingo">Dom.</abbr></th>
             <td>
-              <input type="checkbox" name="domingo-manha" value="domingo-manha" id="domingo-manha" />
+              <input type="checkbox" name="disponibilizade" value="domingo-manha" id="domingo-manha" />
               <label for="domingo-manha" class="screen-reader-only">Manhã, domingo</label>
             </td>
             <td>
-              <input type="checkbox" name="domingo-tarde" value="domingo-tarde" id="domingo-tarde" />
+              <input type="checkbox" name="disponibilizade" value="domingo-tarde" id="domingo-tarde" />
               <label for="domingo-tarde" class="screen-reader-only">Tarde, domingo</label>
             </td>
             <td>
-              <input type="checkbox" name="domingo-noite" value="domingo-noite" id="domingo-noite" />
+              <input type="checkbox" name="disponibilizade" value="domingo-noite" id="domingo-noite" />
               <label for="domingo-noite" class="screen-reader-only">Noite, domingo</label>
             </td>
           </tr>
